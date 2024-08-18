51a055855885f46a725d37459392fda532b4f44d
Akiya <autumnsky64@gmail.com>
2024-08-18 14:49:49 +0900

    Add prism.js code highlighter

--- functions.php
+++ functions.php
@@ -3,8 +3,21 @@ add_action(
 	'wp_enqueue_scripts',
 	function () {
 		wp_enqueue_style(
-			'tsukikusa-nomarize',
+			'tsukikusa',
 			get_stylesheet_uri()
+		);
+
+		wp_enqueue_script(
+			'prism',
+			get_template_directory_uri() . '/js/prism.js',
+			array('jquery'),
+			true
+		);
+
+		wp_enqueue_style(
+			'prism-css',
+			get_template_directory_uri() . '/css/prism.css'
 		);
 	}
 );
+
