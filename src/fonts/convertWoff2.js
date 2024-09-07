72fed68ce90e867d563cf4d049fabc0eec943b4f
Akiya <autumnsky64@gmail.com>
2024-09-07 14:13:26 +0900

    Refactor sorce file dir

--- src/fonts/convertWoff2.js
+++ src/fonts/convertWoff2.js
@@ -3,4 +3,4 @@ import ttf2woff2 from 'ttf2woff2';
 
 const input = await readFile('./src/fonts/monof55.ttf');
 
-await writeFile('./css/fonts/monofur.woff2', ttf2woff2(input));
+await writeFile('../../fonts/monofur.woff2', ttf2woff2(input));
