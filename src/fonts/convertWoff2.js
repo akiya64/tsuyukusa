c8730a56e4928fd70d22ac5829f194c42547ea88
Akiya <autumnsky64@gmail.com>
2024-11-02 20:44:31 +0900

    v0.9.0

--- src/fonts/convertWoff2.js
+++
@@ -1,6 +0,0 @@
-import { readFile, writeFile } from 'node:fs/promises';
-import ttf2woff2 from 'ttf2woff2';
-
-const input = await readFile('./src/fonts/monof55.ttf');
-
-await writeFile('../../fonts/monofur.woff2', ttf2woff2(input));
